f7d8a9a3e994b014d96e66d81c7c7ebd937073e1
Nguyen Hoang Chuong <chuongnh.hcm@gmail.com>
2017-02-22 08:27:37 +0700

    Add project files.

---
+++ chuongnh/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(chuongnh.Startup))]
+namespace chuongnh
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
